28eb8b1d4031622c578b6dcddd349cd6a6a22326
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-18 18:04:46 +0200

    [scientifig] adding ctu8 colormap and simplifying code in color maps.

--- scientifig/color_maps.py
+++ scientifig/color_maps.py
@@ -1,12 +1,47 @@
 import matplotlib.cm
 
+from . import color_constants
+
+# register ctu8 color map
+matplotlib.cm.register_cmap( \
+        name=color_constants.CTU8_CM_NAME, \
+        cmap=matplotlib.colors.ListedColormap( \
+            color_constants.CTU8_LIST, \
+            name=color_constants.CTU8_CM_NAME \
+        ) \
+    )
+
+# covert index into color using colormap and alpha
+def __idx2clr(cmap, idx, alpha):
+    # get particular color from the color map   
+    c = cmap((idx%cmap.N)/cmap.N)
+    # one-liner that extract RGB from color-tuple, add alpha channel, make it tuple again and return
+    return tuple([*c[0:3],alpha])
+
 # improved interface for qualitative color maphs matplotlib colormap
 def get_clr(color_index, dark=True, alpha=1, cmap_name="tab20"):
     # get color map from color maps
     _cmap = matplotlib.cm.get_cmap(cmap_name)
     # define possible shade increment
     _shade_incr = 0 if dark else 1
-    # get particular color from the color map
-    c = _cmap((2*color_index + _shade_incr)/20)
-    # one-liner that extract RGB from color-tuple, add alpha channel, make it tuple again and return
-    return tuple([*c[0:3],alpha])
+    return __idx2clr(_cmap, 2*color_index + _shade_incr, alpha)
+
+# color map based on CTU_blue
+def get_ctu8_clr(color_index, alpha=1):
+    
+    # try to get color map from known color maps ...
+    try:
+        _cmap = matplotlib.cm.get_cmap(color_constants.CTU8_CM_NAME)
+    except ValueError:
+        # ... if fails add it
+        matplotlib.cm.register_cmap( \
+            name=color_constants.CTU8_CM_NAME, \
+            cmap=matplotlib.colors.ListedColormap( \
+                color_constants.CTU8_LIST, \
+                name=color_constants.CTU8_CM_NAME \
+            ) \
+        )
+        _cmap = matplotlib.cm.get_cmap(color_constants.CTU8_CM_NAME)
+    
+    # return converted version
+    return __idx2clr(_cmap, color_index, alpha)
