19f7c364bebd091b35891fb7a00df541d90bb8da
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-19 11:07:55 +0200

    [scientifig] polishing and documenting color_constants.

--- scientifig/color_constants.py
+++ scientifig/color_constants.py
@@ -1,6 +1,7 @@
-
-
-# TODO: clean up
+# Perceptually pleasible colors derived from CTU blue [1] aka Pantone 300 C [2] using Paletton [3]
+# - [1] https://www.cvut.cz/sites/default/files/content/e254fb38-e72d-463b-8c9f-cb0435416f29/cs/20201112-graficky-manual-identity-cvut-v-praze.pdf
+# - [2] https://icolorpalette.com/color/pantone-300-c
+# - [3] https://paletton.com/#uid=13-0u0k++BBy7ZHZD+V+WrG+glw
 
 CTU_BLUE_HEX = "0x005EB8" # defined by CTU
 CTU_ORANGE_HEX = "0xFF9700" # complementary color to CTU_Blue
@@ -15,12 +16,14 @@ CTU_OLIVE_HEX = "0xA7F200" # olive at -80 deg
 #CTU_PINK_HEX = "0xD40081" # pink at 100 deg
 CTU_PINK_HEX = "0xFF009B" # pink at 100 deg
 
+# Hand tuned colors with the base saturation (with the exception of pink that is a bit brighter)
 CTU_CYAN_HEX_alt = "0x00B29F" # hand tuned
 CTU_PURPLE_HEX_alt = "0x7C00BC" # hand tuned
 CTU_PINK_HEX_alt = "0xF65AB5" # hand tuned
 CTU_YELLOW_HEX_alt = "0xFFD900" # hand tuned
 
 """
+# tab20c/tab10c-like color alternatative
 CTU_LIST_HEX =  \
                 [
                     CTU_BLUE_HEX,
@@ -36,6 +39,8 @@ CTU_LIST_HEX =  \
                 ]
 """
 
+# two quadruples of alternating colors
+# '-> very loosely inspired by 'tab10c' sequential color map
 CTU_LIST_HEX =  \
                 [
                     CTU_BLUE_HEX,
@@ -50,20 +55,34 @@ CTU_LIST_HEX =  \
                     "0x000000",
                 ]
 
+# duplication basic ctu color map
+# '-> TODO add brighter alternatibe to each color
+#     '-> it would require different iteration
 CTU20_LIST_HEX = [ c for c in CTU_LIST_HEX for _ in [c,c]]
 
+# this 'private' method convert hexadecimal representation to the color represetation used by MatPlotLib
+# '-> TODO make umprove it 
 def __clr_hex2clr(_hex):
     c = []
+    # converting each digit pairs to separate color
     for i in range(1,4):
         _comp = int(_hex[2*i:2*i+2], base=16)/255.0
         c.append(_comp)
+    # adding alpha at full opacity
     c.append(1)
+    # converting to tuple to meet the required representation
     return tuple(c)
 
+# CTU 20 color list
+# '-> NOTE: not finished
 CTU20_LIST = [__clr_hex2clr(_hex) for _hex in CTU20_LIST_HEX]
 
-CTU16_LIST = CTU20_LIST[0:16]
-CTU8_LIST = [__clr_hex2clr(_hex) for _hex in CTU_LIST_HEX[0:8]]
-
+# CTU 16 color list
+# '-> NOTE: not finished
 CTU16_CM_NAME = "ctu16"
+CTU16_LIST = CTU20_LIST[0:16]
+
+# CTU 8 color list
+# '-> NOTE: kinda finished
 CTU8_CM_NAME = "ctu8"
+CTU8_LIST = [__clr_hex2clr(_hex) for _hex in CTU_LIST_HEX[0:8]]
