5ec1d8eec2ba26c8c3e15bcd87a5843125c10fc2
Jiří Kvant Kubík <jiri.kub@gmail.com>
2023-07-27 22:21:55 +0200

    [scientifig] fixing nameing bug.

--- scientifig/font_settings.py
+++ scientifig/font_settings.py
@@ -1,5 +1,5 @@
-from . import constants
+from . import typography_constants
 import matplotlib.pyplot
 
-def set_recommended_fonts(fonts=constants.IEEE_FONTS):
+def set_recommended_fonts(fonts=typography_constants.IEEE_FONTS):
     matplotlib.pyplot.rcParams.update(fonts)
